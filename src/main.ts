156bbcb447fa7a142b2350a91c6b959017de5cf7
hmachaao <hmachaao@gmail.com>
2019-09-29 16:25:42 +0200

    make use of mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import { app } from './app'
 import * as http from 'http'
 import { MongoHelper } from './mongodb-helper';
+import * as mongoose  from "mongoose";
 
 const PORT = 8080;
+const MONGO_URI = "mongodb://localhost:27017/todo"
 const server = http.createServer(app);
 
 server.listen(PORT);
 server.on('listening', async () => {
     console.log(`Server listening on ${PORT}`);
-    try {
-        await MongoHelper.connect("mongodb://localhost:27017");
-        console.info("Connected to Mongo.");
-    }
-    catch(err) {
+    mongoose.connect(MONGO_URI, {useNewUrlParser: true});
+    mongoose.connection.on('open',() => {
+        console.log("Connected to Mongo.");
+    })
+    mongoose.connection.on('error',(err: any) => {
         console.error(err);
-    }
+    })
 })
